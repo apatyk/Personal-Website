40e585de45cf0f15c24a9a2fce95abb159d4c788
Adam Patyk <apatyk@g.clemson.edu>
2020-04-18 12:23:37 -0400

    FIxing styling on portfolio pages

--- src/templates/portfolio-page.js
+++ src/templates/portfolio-page.js
@@ -5,7 +5,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import CenteredCard from '../components/CenteredCard'
 
-import './portfolio-landing-page.scss'
+import './portfolio-page.scss'
 
 export const PortfolioPageTemplate = ({
   html
