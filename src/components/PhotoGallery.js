3b9f21f00021147b25d0d3854297a392505059b3
Adam Patyk <apatyk@g.clemson.edu>
2020-04-22 20:25:39 -0400

    Adding modals for photos

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import ModalImage from "react-modal-image";
+
 import './PhotoGallery.scss'
 
 const PhotoGallery = ( {
   clImages
 }) => {
+
+    if (!clImages) return <></>
+
     return (
         <div>
           <div className="photo-grid">
             {clImages && clImages.map((image, index) => (
-                <div className="photo-item" key={`${index}-cl`}>
-                  <img src={image.node.secure_url} alt={"no alt :("} />
+                <div className="photo-grid__item" key={`${index}-cl`}>
+                  <ModalImage className="photo-grid__image" small={image.node.secure_url} large={image.node.secure_url} hideDownload={true} hideZoom={true} />
                 </div>
               ))
             }
