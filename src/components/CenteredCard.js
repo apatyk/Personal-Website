7a57a7fa3fe145ff0144bf99ed1db93b9feb78db
Adam Patyk <apatyk@g.clemson.edu>
2020-05-15 13:28:15 -0400

    Cleaning up

--- src/components/CenteredCard.js
+++ src/components/CenteredCard.js
@@ -6,20 +6,22 @@ import Cloud from './Cloud'
 import './CenteredCard.scss'
 
 const CenteredCard = ({ 
-  content, children 
+  content
 }) => {
   return (
+    <>
     <div className="centered-card-container">
       <div 
         className="centered-card" 
         dangerouslySetInnerHTML={{ __html: content }}>
       </div>
-      {children}
       <Cloud top="0" left="-15vw" delay="0s"></Cloud>
       <Cloud top="15vh" left="-15vw" delay="20s"></Cloud>
       <Cloud top="30vh" left="-15vw" delay="10s"></Cloud>
       <Cloud top="45vh" left="-15vw" delay="5s"></Cloud>
     </div>
+    <div className="scroll-indicator"></div>
+    </>
   )
 }
 
