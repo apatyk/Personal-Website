c77c3343fe5be7155bbfef448cefab91c4b25483
Adam Patyk <adamamp@icloud.com>
2024-09-01 22:00:50 -0600

    Add gradient below text on photography page

--- src/components/PhotoBackground.js
+++ src/components/PhotoBackground.js
@@ -11,13 +11,16 @@ const PhotoBackground = ({ clImages }) => {
   const randomImage = clImages[randomPos];
 
   return (
-    <div className="photo-background w-full h-full overflow-hidden absolute top-0">
-      <img
-        className="object-cover w-full h-full"
-        src={randomImage.node.secure_url}
-        alt={randomImage.node.resource_type}
-      />
-    </div>
+    <>
+      <div className="absolute top-0 w-full h-full bg-gradient-to-b from-offwhite from-10% to-50% md:bg-gradient-to-br z-[5]" />
+      <div className="photo-background w-full h-full overflow-hidden absolute top-0">
+        <img
+          className="object-cover w-full h-full"
+          src={randomImage.node.secure_url}
+          alt={randomImage.node.resource_type}
+        />
+      </div>
+    </>
   );
 };
 export default PhotoBackground;
